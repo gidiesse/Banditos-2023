6e3c5ef3657c1ce4f88a272eca0be44f127d44bc
andrea-zanette <zanette482@gmail.com>
2023-07-18 13:00:09 +0200

    Add opt_title

--- utils/plots.py
+++ utils/plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_cumulative_regret(rewards, opt, n_experiments, algorithm_name):
+def plot_cumulative_regret(rewards, opt, n_experiments, algorithm_name, opt_title=""):
     """
     Plot the cumulative regret of the algorithm.
 
@@ -11,11 +11,12 @@ def plot_cumulative_regret(rewards, opt, n_experiments, algorithm_name):
         opt (float): The optimal reward value.
         n_experiments (int): The number of experiments.
         algorithm_name (str): The name of the algorithm.
+        opt_title (str): Optional string appended to the title. Default is empty.
 
     Returns:
         None
     """
-    plt.ylabel("Cumulative regret")
+    plt.ylabel("Cumulative regret "+opt_title)
     plt.xlabel("t")
     cum_regret = np.cumsum(opt - np.array(rewards), axis=1)
     mean_cum_regret = np.mean(cum_regret, axis=0)
@@ -27,7 +28,7 @@ def plot_cumulative_regret(rewards, opt, n_experiments, algorithm_name):
     plt.show()
 
 
-def plot_cumulative_reward(rewards, n_experiments, algorithm_name):
+def plot_cumulative_reward(rewards, n_experiments, algorithm_name, opt_title=""):
     """
         Plot the cumulative reward collected by the algorithm.
 
@@ -35,11 +36,12 @@ def plot_cumulative_reward(rewards, n_experiments, algorithm_name):
             rewards (array-like): The array containing the data to be plotted.
             n_experiments (int): The number of experiments.
             algorithm_name (str): The name of the algorithm.
+            opt_title (str): Optional string appended to the title. Default is empty.
 
         Returns:
             None
     """
-    plt.ylabel("Cumulative reward")
+    plt.ylabel("Cumulative reward "+opt_title)
     plt.xlabel("t")
     cum_reward = np.cumsum(np.array(rewards), axis=1)
     mean_cum_reward = np.mean(cum_reward, axis=0)
@@ -51,7 +53,7 @@ def plot_cumulative_reward(rewards, n_experiments, algorithm_name):
     plt.show()
 
 
-def plot_instantaneous_regret(rewards, opt, n_experiments, algorithm_name):
+def plot_instantaneous_regret(rewards, opt, n_experiments, algorithm_name, opt_title=""):
     """
     Plot the instantaneous regret of the algorithm.
 
@@ -60,11 +62,12 @@ def plot_instantaneous_regret(rewards, opt, n_experiments, algorithm_name):
         opt (float): The optimal reward value.
         n_experiments (int): The number of experiments.
         algorithm_name (str): The name of the algorithm.
+        opt_title (str): Optional string appended to the title. Default is empty.
 
     Returns:
         None
     """
-    plt.ylabel("Instantaneous regret")
+    plt.ylabel("Instantaneous regret "+opt_title)
     plt.xlabel("t")
     inst_regret = (opt - np.array(rewards))
     mean_inst_regret = np.mean(inst_regret, axis=0)
@@ -77,7 +80,7 @@ def plot_instantaneous_regret(rewards, opt, n_experiments, algorithm_name):
     plt.show()
 
 
-def plot_instantaneous_reward(rewards, opt, n_experiments, algorithm_name):
+def plot_instantaneous_reward(rewards, opt, n_experiments, algorithm_name, opt_title=""):
     """
         Plot the instantaneous reward of the algorithm.
 
@@ -86,11 +89,12 @@ def plot_instantaneous_reward(rewards, opt, n_experiments, algorithm_name):
             opt (float): The optimal reward value.
             n_experiments (int): The number of experiments.
             algorithm_name (str): The name of the algorithm.
+            opt_title (str): Optional string appended to the title. Default is empty.
 
         Returns:
             None
     """
-    plt.ylabel("Instantaneous reward")
+    plt.ylabel("Instantaneous reward "+opt_title)
     plt.xlabel("t")
     inst_reward = np.array(rewards)
     mean_inst_reward = np.mean(inst_reward, axis=0)
